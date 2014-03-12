bab7b7f55b83c91d7a37e7981fe599e0c4125db0
triion <triion@gmail.com>
2014-03-12 20:58:58 +0100

    added traceback

--- sickbeard/metadata/mede8er.py
+++ sickbeard/metadata/mede8er.py
@@ -365,7 +365,7 @@ class Mede8erMetadata(generic.GenericMetadata):
                     season_poster = etree.SubElement(season, "poster")
                     season_poster.text = seasons_container[i][j]
         except Exception, e:
-            logger.log("INFO SEASON BANNERS : " + str(seasons_container) +" & "+ etree.tostring(seasons), logger.ERROR)
+            logger.log("INFO SEASON BANNERS : " + ex(e) + traceback.format_exc(), logger.ERROR)
             
         
         logger.log(u"Sending series-data before indentXML: " + etree.tostring(rootNode), logger.DEBUG)
