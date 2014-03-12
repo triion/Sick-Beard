a59706e49986ca971ae9f2fe6255de860e494f1f
triion <triion@gmail.com>
2014-03-12 17:44:43 +0100

    Added seasons xml

--- sickbeard/metadata/mede8er.py
+++ sickbeard/metadata/mede8er.py
@@ -284,7 +284,7 @@ class Mede8erMetadata(generic.GenericMetadata):
 
         rating = etree.SubElement(movie_node, "rating")
         if myShow["rating"] != None:
-            rating.text = str(float(myShow["rating"])*10)
+            rating.text = str(int(float(myShow["rating"])*10))
 
         ratingcount = etree.SubElement(movie_node, "ratingcount")
         if myShow["ratingcount"] != None:
@@ -354,26 +354,26 @@ class Mede8erMetadata(generic.GenericMetadata):
                 cast_actor = etree.SubElement(cast, "actor")
                 cast_actor.text = actor['name']
 
-        #seasons_container = []
-        #image_node = etree.SubElement(movie_node, "image")
-        #if myShow["_banners"] != None:
-        #    for banner in myShow["_banners"]:
-        #        if banner["season"] != None:
-        #            seasons_container[banner["season"]].append(banner)
-        #        else:
-        #            type = banner["BannerType"]
-        #            if banner["BannerType"] == "series":
-        #                type = "banner"
-        #            image = etree.SubElement(image_node, type)
-        #            image.text = banner["BannerPath"]
+        seasons_container = []
+        image_node = etree.SubElement(movie_node, "image")
+        if myShow["_banners"] != None:
+            for banner in myShow["_banners"]:
+                if banner["season"] != None:
+                    seasons_container[banner["season"]].append(banner)
+                else:
+                    type = banner["BannerType"]
+                    if banner["BannerType"] == "series":
+                        type = "banner"
+                    image = etree.SubElement(image_node, type)
+                    image.text = banner["BannerPath"]
 
-        #seasons = etree.SubElement(movie_node, "seasons")
-        #for i in len(seasons_container):
-        #    season = etree.SubElement(seasons, "season")
-        #    season.attrib["number"] = i
-        #    for j in len(seasons_container[i]):
-        #        season_poster = etree.SubElement(season, "poster")
-        #        season_poster.text = seasons_container[i][j]["bannerpath"]
+        seasons = etree.SubElement(movie_node, "seasons")
+        for i in len(seasons_container):
+            season = etree.SubElement(seasons, "season")
+            season.attrib["number"] = i
+            for j in len(seasons_container[i]):
+                season_poster = etree.SubElement(season, "poster")
+                season_poster.text = seasons_container[i][j]["bannerpath"]
         
         logger.log(u"Sending series-data before indentXML: " + etree.tostring(rootNode), logger.DEBUG)
         
