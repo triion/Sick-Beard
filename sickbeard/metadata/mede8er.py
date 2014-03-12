f1d091f7f9d47afa5f77e7fd473ecdae8f5f7943
triion <triion@gmail.com>
2014-03-12 17:36:11 +0100

    Removed rating calculation

--- sickbeard/metadata/mede8er.py
+++ sickbeard/metadata/mede8er.py
@@ -284,7 +284,7 @@ class Mede8erMetadata(generic.GenericMetadata):
 
         rating = etree.SubElement(movie_node, "rating")
         if myShow["rating"] != None:
-            rating.text = 1  #float(myShow["rating"])*10
+            rating.text = myShow["rating"]
 
         ratingcount = etree.SubElement(movie_node, "ratingcount")
         if myShow["ratingcount"] != None:
